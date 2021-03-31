5d0f9cad0a4bcd0c76b773780db5aca29bce89e2
Aaron <aaronwenteler@gmail.com>
2021-04-01 00:06:46 +0200

    Revert "Some example functions"
    This reverts commit 0ab08e26c9f826498d11584310d49cc3a4a3074a.

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-import numpy as np
 
 
 def print_hi(name):
-    text = 'Hi, %s!' % name
 
-    return text
-
-
-def avg_calc(*args):
-    stat = (sum(list(args)))/len(list(args))
-
-    return stat
+    print('Hi, {name}')
+    print(1+1)
 
 
 if __name__ == '__main__':
-    print(print_hi('Alice'))
-    print(avg_calc(7, 8, 6))
+    print_hi('Hello World!')
+
 

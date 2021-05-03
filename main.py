7a326ca88fee952e3ae4842c01f55ba3b35bf2dc
Tygo <postvoortygo@hotmail.nl>
2021-05-03 13:42:36 +0200

    numerical stability
    fixed numerical stability issues and fixed some mistakes

--- main.py
+++ main.py
@@ -1,59 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def heatmap2d(arr: np.ndarray):
-    plt.imshow(arr, cmap='viridis')
-    plt.colorbar()
-    plt.show()
+# This code is exclusively for a 1D simulation
+
+
+
 
 def petri_rod(b,l,p,Vc, Vv):
     
     
-    DBmax   = 5.25 * 10**5 * um_to_m**2 /h_to_s   # um^2/h to m^2/s
-    Kv      = .001 * (10**(-6))**(-2)             # um^-2 to m^-2
-    Kc      = .02 * (10**(-6))**(-2)              # um^-2 to m^-2
-    alphac  = 2                                   #
-    Dp      = 1 * ((10**(-6)))**2 /3600           # um^2/h to m^2/s
-    Dn      = 4.5*10**6 * um_to_m**2 /h_to_s      #
-    eta     = 8 * 10**4 * um_to_m**2 /h_to_s      # um^2/h to m^2/s
-    beta    = 80                                  #
-    kl      = 2 * um_to_m**2 /h_to_s              # um^2/h to m^2/s
-    gmax    = 6 / h_to_s                          # 1/h to 1/s
-    Kn      =  .1 * um_to_m**(-2)                 # um^-2 to m^-2
-    Lambda  = .2                                  # 
-    Kb      =  .1 * um_to_m**(-2)                 # um^-2 to m^-2
-    B0      = 10**7                               #
-    P0      = 1.5 * 10**8                         #
-    n0      = 1 * um_to_m**(-2)                   # um^-2 to m^-2
-    r       = .25 * cm_to_m                       # cm to m
-    R       = 5.5 * cm_to_m                       # cm to m
+           
     
+    B = np.log(b)
+    L = np.log(l)
+    P = np.log(p)
+    [Drate,Dpr,G,Omega,h,Theta,Length_petri] = Vc  
     
-    DBmax   = 5.25 * 10**5  
-    Kv      = .001            
-    Kc      = .02             
-    alphac  = 2                                   #
-    Dp      = 1          # um^2/h to m^2/s
-    Dn      = 4.5*10**6    #
-    eta     = 8 * 10**4     # um^2/h to m^2/s
-    beta    = 80                                  #
-    kl      = 2             # um^2/h to m^2/s
-    gmax    = 6                         # 1/h to 1/s
-    Kn      =  .1                # um^-2 to m^-2
-    Lambda  = .2                                  # 
-    Kb      =  .1                # um^-2 to m^-2
-    B0      = 10**7                               #
-    P0      = 1.5 * 10**8                         #
-    n0      = 1                  
-    r       = .25 * 1000                   
-    R       = 5.5 *  1000                      
-    
-    B = np.log(b); L = np.log(l); P = np.log(p); 
-    
-    
+    [N,dt,dur, b_min, l_min, p_min] = Vv
 
     
-    dx = 2*R/(N+1)
+    
     
     # Left_BOperator = [((B[1]-B[0]+2)**2-4)/(4*dx**2)]
     # Mid_BOperator = ((B[2:N] - B[0:N-2] + 2)**2 + 8*(B[0:N-2] -  B[1:N-1]) -4)/(4*dx**2)
@@ -73,52 +39,53 @@ def petri_rod(b,l,p,Vc, Vv):
     
     # POperator = np.concatenate((Left_POperator,Mid_POperator,Right_POperator))
     
-    Drate = DBmax * (n0/(n0 +Kv))**2 # m^2/s
-    G = gmax * n0 / (n0 + Kn) /Drate
-    Omega = kl * n0 / Drate
-    Dpr = Dp  / Drate
-    h = eta * Kb / Drate
-    Theta  = beta * kl * n0 * eta * Kb / Drate**2
+    dx = Length_petri/N
     
-    Steps = np.round(dur/dt)
+    #print(dx)
     
-    # tau = Dtau * Drate * t
-    
-    #Steps = Dtau * Drate * t / Dtau = t * Drate ? 
-    Steps = 1
+    Steps = np.round(dur/(dt*samples))
+    #print(Steps)
+    Dtau = Drate * dt # um^2
+    #print(Dtau)
+    #Steps = 1
     i = 0 
     
     while i <  Steps: 
         
         T = np.exp(B)+np.exp(L)
-        F = np.divide(np.exp(P),T+Kb)
+        F = np.divide(np.exp(P),np.exp(B)+np.exp(L)+Kb)
         F_log = P - np.log(T+Kb)
         Bla_log = B-L+ P - np.log(np.exp(B)+np.exp(L)+Kb)
         Bla = np.exp(Bla_log)
-        print('B')
-        print(B)
-        print('L')
-        print(L)
-        print('T')
-        print(T)
-        print('F')
-        print(F)
-        print('B-L ')
-        print(B-L)
-        print('np.exp(B-L)*F ')
-        print(np.exp(B-L)*F )
-    
+        # print('B')
+        # print(B)
+        # print('L')
+        # print(L)
+        # print('P')
+        # print(P)
+        # print('L plus')
+        # print(Dtau * (np.divide(1,np.exp(L-P)+np.exp(2*L-B-P)+Kb/np.exp(B+P-L)) - Omega + np.concatenate(([((L[1]-L[0]+2)**2-4)],((L[2:N] - L[0:N-2] + 2)**2 + 8*(L[0:N-2] -  L[1:N-1]) -4),[((L[N-2]-L[N-1]+2)**2-4)]))/(4*dx**2) ))
+         # B1 = B + G - np.divide(np.exp(P),np.exp(B)+np.exp(L)+Kb) + np.concatenate(([((B[1]-B[0]+2)**2-4)],((B[2:N] - B[0:N-2] + 2)**2 + 8*(B[0:N-2] -  B[1:N-1]) -4),[((B[N-2]-B[N-1]+2)**2-4)]))/(4*dx**2)
+        # L1 = L + np.exp(B-L)*np.divide(np.exp(P),np.exp(B)+np.exp(L)+Kb) - Omega + np.concatenate(([((L[1]-L[0]+2)**2-4)],((L[2:N] - L[0:N-2] + 2)**2 + 8*(L[0:N-2] -  L[1:N-1]) -4),[((L[N-2]-L[N-1]+2)**2-4)]))/(4*dx**2)
+        # P1 = P - np.divide(T*h,T+Kb) + np.exp(L-P) * Theta + Dpr * np.concatenate(([((P[1]-P[0]+2)**2-4)],((P[2:N] - P[0:N-2] + 2)**2 + 8*(P[0:N-2] -  P[1:N-1]) -4),[((P[N-2]-P[N-1]+2)**2-4)]))/(4*dx**2)
+        
         # B1 = B + G - F + np.concatenate(([((B[1]-B[0]+2)**2-4)],((B[2:N] - B[0:N-2] + 2)**2 + 8*(B[0:N-2] -  B[1:N-1]) -4),[((B[N-2]-B[N-1]+2)**2-4)]))/(4*dx**2)
         # L1 = L + np.exp(B-L)*F - Omega + np.concatenate(([((L[1]-L[0]+2)**2-4)],((L[2:N] - L[0:N-2] + 2)**2 + 8*(L[0:N-2] -  L[1:N-1]) -4),[((L[N-2]-L[N-1]+2)**2-4)]))/(4*dx**2)
-        # P1 = P + np.divide(T*h,T+Kb) + np.exp(L-P) * Theta + Dpr * np.concatenate(([((P[1]-P[0]+2)**2-4)],((P[2:N] - P[0:N-2] + 2)**2 + 8*(P[0:N-2] -  P[1:N-1]) -4),[((P[N-2]-P[N-1]+2)**2-4)]))/(4*dx**2)
+        # P1 = P - np.divide((np.exp(B)+np.exp(L))*h,np.exp(B)+np.exp(L)+Kb) + np.exp(L-P) * Theta + Dpr * np.concatenate(([((P[1]-P[0]+2)**2-4)],((P[2:N] - P[0:N-2] + 2)**2 + 8*(P[0:N-2] -  P[1:N-1]) -4),[((P[N-2]-P[N-1]+2)**2-4)]))/(4*dx**2)
         
         
-        B1 = B + G - F + np.concatenate(([((B[1]-B[0]+2)**2-4)],((B[2:N] - B[0:N-2] + 2)**2 + 8*(B[0:N-2] -  B[1:N-1]) -4),[((B[N-2]-B[N-1]+2)**2-4)]))/(4*dx**2)
-        L1 = L + np.exp(B-L+ P - np.log(np.exp(B)+np.exp(L)+Kb)) - Omega + np.concatenate(([((L[1]-L[0]+2)**2-4)],((L[2:N] - L[0:N-2] + 2)**2 + 8*(L[0:N-2] -  L[1:N-1]) -4),[((L[N-2]-L[N-1]+2)**2-4)]))/(4*dx**2)
-        P1 = P + np.divide(T*h,T+Kb) + np.exp(L-P) * Theta + Dpr * np.concatenate(([((P[1]-P[0]+2)**2-4)],((P[2:N] - P[0:N-2] + 2)**2 + 8*(P[0:N-2] -  P[1:N-1]) -4),[((P[N-2]-P[N-1]+2)**2-4)]))/(4*dx**2)
+        B1 = B + Dtau *(G - np.divide(1,np.exp(B-P)+np.exp(L-P)+Kb/np.exp(P)) + np.concatenate(([((B[1]-B[0]+2)**2-4)],((B[2:N] - B[0:N-2] + 2)**2 + 8*(B[0:N-2] -  B[1:N-1]) -4),[((B[N-2]-B[N-1]+2)**2-4)]))/(4*dx**2) )
+        L1 = L + Dtau * (np.divide(1,np.exp(L-P)+np.exp(2*L-B-P)+Kb/np.exp(B+P-L)) - Omega + np.concatenate(([((L[1]-L[0]+2)**2-4)],((L[2:N] - L[0:N-2] + 2)**2 + 8*(L[0:N-2] -  L[1:N-1]) -4),[((L[N-2]-L[N-1]+2)**2-4)]))/(4*dx**2) )
+        P1 = P - Dtau *(np.divide(h,1+Kb/((np.exp(B)+np.exp(L)))) + np.exp(L-P) * Theta + Dpr * np.concatenate(([((P[1]-P[0]+2)**2-4)],((P[2:N] - P[0:N-2] + 2)**2 + 8*(P[0:N-2] -  P[1:N-1]) -4),[((P[N-2]-P[N-1]+2)**2-4)]))/(4*dx**2) )
         
         
-        B , L , P = B1 , L1 , P1
+        B = B1
+        L = L1
+        P = P1
+        B = np.where(B < b_min, b_min, B)
+        L = np.where(L < l_min, l_min, L)
+        P = np.where(P < p_min, p_min, P)
+
         
         i += 1
     
@@ -131,102 +98,126 @@ def petri_rod(b,l,p,Vc, Vv):
     
     return np.exp(B) , np.exp(L) , np.exp(P)
 
-# we do all units in SI units 
+# # we do all units in SI units 
 
-um_to_m = 10**(-6)
-cm_to_m = 10**(-3)
-h_to_s  = 3600
-
-
-#--------------parameters---------------------- 
-
-DBmax   = 5.25 * 10**5 * um_to_m**2 /h_to_s   # um^2/h to m^2/s
-Kv      = .001 * (10**(-6))**(-2)             # um^-2 to m^-2
-Kc      = .02 * (10**(-6))**(-2)              # um^-2 to m^-2
-alphac  = 2                                   #
-Dp      = 1 * ((10**(-6)))**2 /3600           # um^2/h to m^2/s
-Dn      = 4.5*10**6 * um_to_m**2 /h_to_s      #
-eta     = 8 * 10**4 * um_to_m**2 /h_to_s      # um^2/h to m^2/s
-beta    = 80                                  #
-kl      = 2 * um_to_m**2 /h_to_s              # um^2/h to m^2/s
-gmax    = 6 / h_to_s                          # 1/h to 1/s
-Kn      =  .1 * um_to_m**(-2)                 # um^-2 to m^-2
-Lambda  = .2                                  # 
-Kb      =  .1 * um_to_m**(-2)                 # um^-2 to m^-2
-B0      = 10**7                               #
-P0      = 1.5 * 10**8                         #
-n0      = 1 * um_to_m**(-2)                   # um^-2 to m^-2
-r       = .25 * cm_to_m                       # cm to m
-R       = 5.5 * cm_to_m                       # cm to m
+# um_to_m = 10**(-6)
+# cm_to_m = 10**(-3)
+# h_to_s  = 3600
 
 
-DBmax   = 5.25 * 10**5  
-Kv      = .001            
-Kc      = .02             
-alphac  = 2                                   #
-Dp      = 1          # um^2/h to m^2/s
-Dn      = 4.5*10**6    #
-eta     = 8 * 10**4     # um^2/h to m^2/s
-beta    = 80                                  #
-kl      = 2             # um^2/h to m^2/s
-gmax    = 6                         # 1/h to 1/s
-Kn      =  .1                # um^-2 to m^-2
-Lambda  = .2                                  # 
-Kb      =  .1                # um^-2 to m^-2
-B0      = 10**7                               #
-P0      = 1.5 * 10**8                         #
-n0      = 1                  
-r       = .25 * 1000                   
-R       = 5.5 *  1000 
+# #--------------parameters---------------------- 
 
+# DBmax   = 5.25 * 10**5 * um_to_m**2 /h_to_s   # um^2/h to m^2/s
+# Kv      = .001 * (10**(-6))**(-2)             # um^-2 to m^-2
+# Kc      = .02 * (10**(-6))**(-2)              # um^-2 to m^-2
+# alphac  = 2                                   #
+# Dp      = 1 * ((10**(-6)))**2 /3600           # um^2/h to m^2/s
+# Dn      = 4.5*10**6 * um_to_m**2 /h_to_s      #
+# eta     = 8 * 10**4 * um_to_m**2 /h_to_s      # um^2/h to m^2/s
+# beta    = 80                                  #
+# kl      = 2 * um_to_m**2 /h_to_s              # um^2/h to m^2/s
+# gmax    = 6 / h_to_s                          # 1/h to 1/s
+# Kn      =  .1 * um_to_m**(-2)                 # um^-2 to m^-2
+# Lambda  = .2                                  # 
+# Kb      =  .1 * um_to_m**(-2)                 # um^-2 to m^-2
+# B0      = 10**7                               #
+# P0      = 1.5 * 10**8                         #
+# n0      = 1 * um_to_m**(-2)                   # um^-2 to m^-2
+# r       = .25 * cm_to_m                       # cm to m
+# R       = 5.5 * cm_to_m                       # cm to m
+
+    
+DBmax   = 5.25 * 10**5  # um^2/h
+Kv      = .001          # um^-2
+Kc      = .02           # um^-2
+alphac  = 2             # 1
+Dp      = 1             # um^2/h
+Dn      = 4.5*10**6     # um^2/h
+eta     = 8 * 10**4     # um^2/h
+beta    = 80            # 1              
+kl      = 2             # um^2/h
+gmax    = 6             # 1/h
+Kn      =  .1           # um^-2
+Lambda  = .2            # 1 
+Kb      =  .1           # um^-2
+B0      = 10**7         # 1
+P0      = 1.5 * 10**8   # 1
+n0      = 1             # um^-2
+Length_petri = 1000     # um   
+B_0 = 1                 # um^-2
+L_0 = 1                 # um^-2
+P_0 = 1                 # um^-2
+B_min = 10**(-10)        # um^-2 = 1 cm^-2
+L_min = 10**(-10)        # um^-2 = 1 cm^-2
+P_min = 10**(-10)        # um^-2 = 1 cm^-2
+
+Drate = DBmax * (n0/(n0 +Kv))**2                # um^2/h
+Dpr = Dp  / Drate                               # 1
+G = gmax * n0 / (n0 + Kn) /Drate                # um^-2
+Omega = kl * n0 / Drate                         # um^-2
+h = eta * Kb / Drate                            # um^-2
+Theta  = beta * kl * n0 * eta * Kb / Drate**2   # um^-4
 
 #vector of constants
-Vc = [DBmax,Kv,Kc,alphac,Dp,Dn,eta,beta,kl,gmax,Kn,Lambda,Kb,B0,P0,n0,r,R]
+Vc = [Drate,Dpr,G,Omega,h,Theta,Length_petri]
 #--------------Condensed constants---------------------- 
 
-# Drate = DBmax * (n0/(n0 +Kv))**2
-# G = gmax * n0 / (n0 + Kn) /Drate
-# Omega = kl * n0 / Drate
-# Dpr = Dp  / Drate
-# h = eta * Kb / Drate
-# Theta  = beta * kl * n0 * eta * Kb / Drate**2
+
 
 
 
 #------------------Simulation Variables---------------------
 
 N = 10 #discretisaion in X (N)
-dt = 10**(-3)  #time step
-dur = 10 # duration
-samples = 10 #how often one wants data to be returned
+dt = 10**(-4)  #time steps in h 
+dur = 3 # duration of total simulation in h
+samples = 30 #how often one wants data to be returned
 
-bresults = np.zeros((N,samples+1))
-lresults = np.zeros((N,samples+1))
-presults = np.zeros((N,samples+1))
+Bresults = np.zeros((N,samples+1))
+Lresults = np.zeros((N,samples+1))
+Presults = np.zeros((N,samples+1))
 
-# parameters for bacteria, infected bacteria and phages
+# parameters for bacteria, infected bacteria and phages in # um^-2
 min_val = np.finfo(float).eps
-b = min_val*np.ones((N)) ; l = min_val*np.ones((N)) ; p = min_val*np.ones((N))
-b = np.ones((N)) ; l = np.ones((N))*10**(-4) ; p = np.ones((N)) 
-#vector of variables
-Vv = [N,dt,dur]
+B = min_val*np.ones((N)) ; L = min_val*np.ones((N)) ; P = min_val*np.ones((N))
+B = np.cumsum(np.ones((N))) ; L = np.ones((N))*10**(-6) ; P = np.ones((N))*10**(-6)
 
-bresults[:,0] , lresults[:,0]  , presults[:,0]  = b , l , p
+# convert BLP to blp
+
+b = B/B_0 
+b_min = np.log(B_min/B_0)
+l = L/L_0
+l_min = np.log(L_min/L_0)
+p = P*h/P_0
+p_min = np.log(P_min*h/P_0)
+
+
+
+#vector of variables
+Vv = [N,dt,dur, b_min, l_min, p_min]
+
+Bresults[:,0] , Lresults[:,0]  , Presults[:,0]  = B , L , P
 
 
 for i in range(samples):
     b,l,p = petri_rod(b,l,p,Vc, Vv)
-    bresults[:,i+1] , lresults[:,i+1]  , presults[:,i+1]  = b , l , p
+    Bresults[:,i+1] , Lresults[:,i+1]  , Presults[:,i+1]  = b*B_0 , l*L_0 , p*P_0/h
 
 
-#heatmap2d(bresults)
-heatmap2d(lresults)
-#heatmap2d(presults)
+
+fig, axs = plt.subplots(3)
+fig.suptitle('results over time')
+axs[0].imshow(Bresults, cmap='viridis')
+axs[1].imshow(Lresults, cmap='viridis')
+axs[2].imshow(Presults, cmap='viridis')
+
+#fig.colorbar()
+plt.show()
 
 print('the end results')
-print(b)
-print(l)
-print(p)
+print(b*B_0)
+print(l*L_0)
+print(p*P_0/h)
 
 #F = np.divide(p,b+l+Kb)
 

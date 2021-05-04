02dc4ede42a094d28033bb1a289cc963d3c6cb5e
Tygo <postvoortygo@hotmail.nl>
2021-05-04 13:27:45 +0200

    The simulation works?
    The simulation works for 1D, so it seems. Time to optimise and expand.

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ def petri_rod(b,l,p,Vc, Vv):
     # POperator = np.concatenate((Left_POperator,Mid_POperator,Right_POperator))
     
     dx = Length_petri/N
+    DX = 1/(4*dx**2)
     
     #print(dx)
     
@@ -50,22 +51,15 @@ def petri_rod(b,l,p,Vc, Vv):
     #Steps = 1
     i = 0 
     
+    # DtauG = Dtau * G
+    # DtauDX = DX * Dtau
+    # DtauDXDpr = DtauDX * Dpr
+    # Dtauh = Dtau*h
+    # DtauTheta = Dtau*Theta
+    
     while i <  Steps: 
         
-        T = np.exp(B)+np.exp(L)
-        F = np.divide(np.exp(P),np.exp(B)+np.exp(L)+Kb)
-        F_log = P - np.log(T+Kb)
-        Bla_log = B-L+ P - np.log(np.exp(B)+np.exp(L)+Kb)
-        Bla = np.exp(Bla_log)
-        # print('B')
-        # print(B)
-        # print('L')
-        # print(L)
-        # print('P')
-        # print(P)
-        # print('L plus')
-        # print(Dtau * (np.divide(1,np.exp(L-P)+np.exp(2*L-B-P)+Kb/np.exp(B+P-L)) - Omega + np.concatenate(([((L[1]-L[0]+2)**2-4)],((L[2:N] - L[0:N-2] + 2)**2 + 8*(L[0:N-2] -  L[1:N-1]) -4),[((L[N-2]-L[N-1]+2)**2-4)]))/(4*dx**2) ))
-         # B1 = B + G - np.divide(np.exp(P),np.exp(B)+np.exp(L)+Kb) + np.concatenate(([((B[1]-B[0]+2)**2-4)],((B[2:N] - B[0:N-2] + 2)**2 + 8*(B[0:N-2] -  B[1:N-1]) -4),[((B[N-2]-B[N-1]+2)**2-4)]))/(4*dx**2)
+        # B1 = B + G - np.divide(np.exp(P),np.exp(B)+np.exp(L)+Kb) + np.concatenate(([((B[1]-B[0]+2)**2-4)],((B[2:N] - B[0:N-2] + 2)**2 + 8*(B[0:N-2] -  B[1:N-1]) -4),[((B[N-2]-B[N-1]+2)**2-4)]))/(4*dx**2)
         # L1 = L + np.exp(B-L)*np.divide(np.exp(P),np.exp(B)+np.exp(L)+Kb) - Omega + np.concatenate(([((L[1]-L[0]+2)**2-4)],((L[2:N] - L[0:N-2] + 2)**2 + 8*(L[0:N-2] -  L[1:N-1]) -4),[((L[N-2]-L[N-1]+2)**2-4)]))/(4*dx**2)
         # P1 = P - np.divide(T*h,T+Kb) + np.exp(L-P) * Theta + Dpr * np.concatenate(([((P[1]-P[0]+2)**2-4)],((P[2:N] - P[0:N-2] + 2)**2 + 8*(P[0:N-2] -  P[1:N-1]) -4),[((P[N-2]-P[N-1]+2)**2-4)]))/(4*dx**2)
         
@@ -73,15 +67,24 @@ def petri_rod(b,l,p,Vc, Vv):
         # L1 = L + np.exp(B-L)*F - Omega + np.concatenate(([((L[1]-L[0]+2)**2-4)],((L[2:N] - L[0:N-2] + 2)**2 + 8*(L[0:N-2] -  L[1:N-1]) -4),[((L[N-2]-L[N-1]+2)**2-4)]))/(4*dx**2)
         # P1 = P - np.divide((np.exp(B)+np.exp(L))*h,np.exp(B)+np.exp(L)+Kb) + np.exp(L-P) * Theta + Dpr * np.concatenate(([((P[1]-P[0]+2)**2-4)],((P[2:N] - P[0:N-2] + 2)**2 + 8*(P[0:N-2] -  P[1:N-1]) -4),[((P[N-2]-P[N-1]+2)**2-4)]))/(4*dx**2)
         
+        LminP = L - P
+        B1 = B + Dtau *(G - np.divide(1,np.exp(B-P)+np.exp(LminP)+Kb/np.exp(P)) + np.concatenate(([((B[1]-B[0]+2)**2-4)],((B[2:N] - B[0:N-2] + 2)**2 + 8*(B[0:N-2] -  B[1:N-1]) -4),[((B[N-2]-B[N-1]+2)**2-4)]))*DX )
+        L1 = L + Dtau * (np.divide(1,np.exp(LminP)+np.exp(L-B+LminP)+Kb/np.exp(B-LminP)) - Omega + np.concatenate(([((L[1]-L[0]+2)**2-4)],((L[2:N] - L[0:N-2] + 2)**2 + 8*(L[0:N-2] -  L[1:N-1]) -4),[((L[N-2]-L[N-1]+2)**2-4)]))*DX )
+        P = P - Dtau *(np.divide(h,1+Kb/((np.exp(B)+np.exp(L)))) + np.exp(LminP) * Theta + Dpr * np.concatenate(([((P[1]-P[0]+2)**2-4)],((P[2:N] - P[0:N-2] + 2)**2 + 8*(P[0:N-2] -  P[1:N-1]) -4),[((P[N-2]-P[N-1]+2)**2-4)]))*DX )
         
-        B1 = B + Dtau *(G - np.divide(1,np.exp(B-P)+np.exp(L-P)+Kb/np.exp(P)) + np.concatenate(([((B[1]-B[0]+2)**2-4)],((B[2:N] - B[0:N-2] + 2)**2 + 8*(B[0:N-2] -  B[1:N-1]) -4),[((B[N-2]-B[N-1]+2)**2-4)]))/(4*dx**2) )
-        L1 = L + Dtau * (np.divide(1,np.exp(L-P)+np.exp(2*L-B-P)+Kb/np.exp(B+P-L)) - Omega + np.concatenate(([((L[1]-L[0]+2)**2-4)],((L[2:N] - L[0:N-2] + 2)**2 + 8*(L[0:N-2] -  L[1:N-1]) -4),[((L[N-2]-L[N-1]+2)**2-4)]))/(4*dx**2) )
-        P1 = P - Dtau *(np.divide(h,1+Kb/((np.exp(B)+np.exp(L)))) + np.exp(L-P) * Theta + Dpr * np.concatenate(([((P[1]-P[0]+2)**2-4)],((P[2:N] - P[0:N-2] + 2)**2 + 8*(P[0:N-2] -  P[1:N-1]) -4),[((P[N-2]-P[N-1]+2)**2-4)]))/(4*dx**2) )
+        
+        
+        
+        # LminP = L - P
+        # B1 = B +  DtauG - np.divide(Dtau,np.exp(B-P)+np.exp(LminP)+Kb/np.exp(P)) + np.concatenate(([((B[1]-B[0]+2)**2-4)],((B[2:N] - B[0:N-2] + 2)**2 + 8*(B[0:N-2] -  B[1:N-1]) -4),[((B[N-2]-B[N-1]+2)**2-4)]))*DtauDX 
+        # L1 = L + np.divide(1,np.exp(LminP)+np.exp(L-B+LminP)+Kb/np.exp(B-LminP)) - Omega + np.concatenate(([((L[1]-L[0]+2)**2-4)],((L[2:N] - L[0:N-2] + 2)**2 + 8*(L[0:N-2] -  L[1:N-1]) -4),[((L[N-2]-L[N-1]+2)**2-4)]))*DtauDX 
+        # P = P - np.divide(Dtauh,1+Kb/((np.exp(B)+np.exp(L)))) + np.exp(LminP) * DtauTheta + np.concatenate(([((P[1]-P[0]+2)**2-4)],((P[2:N] - P[0:N-2] + 2)**2 + 8*(P[0:N-2] -  P[1:N-1]) -4),[((P[N-2]-P[N-1]+2)**2-4)]))*DtauDXDpr
+        
         
         
         B = B1
         L = L1
-        P = P1
+        
         B = np.where(B < b_min, b_min, B)
         L = np.where(L < l_min, l_min, L)
         P = np.where(P < p_min, p_min, P)
@@ -89,43 +92,14 @@ def petri_rod(b,l,p,Vc, Vv):
         
         i += 1
     
-    # print(B)
-    # print(L)
-    # print(P)
-    
     
     
     
     return np.exp(B) , np.exp(L) , np.exp(P)
 
-# # we do all units in SI units 
-
-# um_to_m = 10**(-6)
-# cm_to_m = 10**(-3)
-# h_to_s  = 3600
 
 
 # #--------------parameters---------------------- 
-
-# DBmax   = 5.25 * 10**5 * um_to_m**2 /h_to_s   # um^2/h to m^2/s
-# Kv      = .001 * (10**(-6))**(-2)             # um^-2 to m^-2
-# Kc      = .02 * (10**(-6))**(-2)              # um^-2 to m^-2
-# alphac  = 2                                   #
-# Dp      = 1 * ((10**(-6)))**2 /3600           # um^2/h to m^2/s
-# Dn      = 4.5*10**6 * um_to_m**2 /h_to_s      #
-# eta     = 8 * 10**4 * um_to_m**2 /h_to_s      # um^2/h to m^2/s
-# beta    = 80                                  #
-# kl      = 2 * um_to_m**2 /h_to_s              # um^2/h to m^2/s
-# gmax    = 6 / h_to_s                          # 1/h to 1/s
-# Kn      =  .1 * um_to_m**(-2)                 # um^-2 to m^-2
-# Lambda  = .2                                  # 
-# Kb      =  .1 * um_to_m**(-2)                 # um^-2 to m^-2
-# B0      = 10**7                               #
-# P0      = 1.5 * 10**8                         #
-# n0      = 1 * um_to_m**(-2)                   # um^-2 to m^-2
-# r       = .25 * cm_to_m                       # cm to m
-# R       = 5.5 * cm_to_m                       # cm to m
-
     
 DBmax   = 5.25 * 10**5  # um^2/h
 Kv      = .001          # um^-2
@@ -135,7 +109,7 @@ Dp      = 1             # um^2/h
 Dn      = 4.5*10**6     # um^2/h
 eta     = 8 * 10**4     # um^2/h
 beta    = 80            # 1              
-kl      = 2             # um^2/h
+kl      = 2#00             # um^2/h
 gmax    = 6             # 1/h
 Kn      =  .1           # um^-2
 Lambda  = .2            # 1 
@@ -147,9 +121,12 @@ Length_petri = 1000     # um
 B_0 = 1                 # um^-2
 L_0 = 1                 # um^-2
 P_0 = 1                 # um^-2
-B_min = 10**(-10)        # um^-2 = 1 cm^-2
-L_min = 10**(-10)        # um^-2 = 1 cm^-2
-P_min = 10**(-10)        # um^-2 = 1 cm^-2
+B_min = 10**(-10)       # um^-2 = 1 cm^-2
+L_min = 10**(-10)       # um^-2 = 1 cm^-2
+P_min = 10**(-10)       # um^-2 = 1 cm^-2
+
+
+#--------------Condensed constants---------------------- 
 
 Drate = DBmax * (n0/(n0 +Kv))**2                # um^2/h
 Dpr = Dp  / Drate                               # 1
@@ -160,17 +137,13 @@ Theta  = beta * kl * n0 * eta * Kb / Drate**2   # um^-4
 
 #vector of constants
 Vc = [Drate,Dpr,G,Omega,h,Theta,Length_petri]
-#--------------Condensed constants---------------------- 
-
-
-
 
 
 #------------------Simulation Variables---------------------
 
 N = 10 #discretisaion in X (N)
-dt = 10**(-4)  #time steps in h 
-dur = 3 # duration of total simulation in h
+dt = 10**(-8)  #time steps in h 
+dur = .0030 # duration of total simulation in h
 samples = 30 #how often one wants data to be returned
 
 Bresults = np.zeros((N,samples+1))
@@ -180,8 +153,8 @@ Presults = np.zeros((N,samples+1))
 # parameters for bacteria, infected bacteria and phages in # um^-2
 min_val = np.finfo(float).eps
 B = min_val*np.ones((N)) ; L = min_val*np.ones((N)) ; P = min_val*np.ones((N))
-B = np.cumsum(np.ones((N))) ; L = np.ones((N))*10**(-6) ; P = np.ones((N))*10**(-6)
-
+B = np.ones((N)) ; L = np.ones((N))*10**(-6) ; P = np.ones((N))*10**(-2)
+B[0] = 100
 # convert BLP to blp
 
 b = B/B_0 
@@ -207,9 +180,15 @@ for i in range(samples):
 
 fig, axs = plt.subplots(3)
 fig.suptitle('results over time')
-axs[0].imshow(Bresults, cmap='viridis')
-axs[1].imshow(Lresults, cmap='viridis')
-axs[2].imshow(Presults, cmap='viridis')
+axs[0].imshow(np.log(Bresults), cmap='viridis')
+axs[0].set_title('Bacteria')
+axs[0].set(ylabel='Distance')
+axs[1].imshow(np.log(Lresults), cmap='viridis')
+axs[1].set_title('Infected bacteria')
+axs[1].set(ylabel='Distance')
+axs[2].imshow(np.log(Presults), cmap='viridis')
+axs[2].set_title('Phages')
+axs[2].set(xlabel='Time', ylabel='Distance')
 
 #fig.colorbar()
 plt.show()
